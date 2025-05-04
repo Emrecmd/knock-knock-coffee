21ed569f7566a255c904fd1a97cc4f423ace7911
Emre <emreerenkizildag@gmail.com>
2025-05-04 18:49:54 +0300

    tarih ve saat artık datepicker ile alınıyor

--- final_version_2025_05_04_13_26_52/sketch.js
+++ final_version_2025_05_04_13_26_52/sketch.js
@@ -80,8 +80,7 @@ function draw() {
     text(`Seçilen Yer: ${selectedLocation}`, width / 2, height / 2 - 140);
     textSize(18);
 
-    drawInputBox(width / 2 - 100, height / 2 - 40, 200, 40, selectedDate, "YYYY-AA-GG", typingDate);
-    drawInputBox(width / 2 - 100, height / 2 + 20, 200, 40, selectedTime, "SS:DD", typingTime);
+    drawDateTimePicker();
     
     drawButton(onaylaButtonBox.x, onaylaButtonBox.y, onaylaButtonBox.w, onaylaButtonBox.h, "Onayla");
   }
@@ -110,13 +109,25 @@ function drawLocationOptions() {
   }
 }
 
-function drawInputBox(x, y, w, h, value, placeholder, active) {
-  fill(active ? '#FFFFFF' : '#FFD1DC');
-  rect(x, y, w, h, 10);
+function drawDateTimePicker() {
+  fill('#FFD1DC');
+  rect(width / 2 - 100, height / 2 - 40, 200, 40, 10);
   fill('#001f3f');
   textAlign(LEFT, CENTER);
-  text(value || placeholder, x + 10, y + h / 2);
-  textAlign(CENTER, CENTER);
+  text("Tarih: " + selectedDate, width / 2 - 90, height / 2 - 40 + 20);
+  
+  let datePicker = createInput(selectedDate, "date");
+  datePicker.position(width / 2 - 100, height / 2 - 40);
+  datePicker.size(200, 30);
+  datePicker.input(() => selectedDate = datePicker.value());
+
+  rect(width / 2 - 100, height / 2 + 20, 200, 40, 10);
+  text("Saat: " + selectedTime, width / 2 - 90, height / 2 + 20 + 20);
+  
+  let timePicker = createInput(selectedTime, "time");
+  timePicker.position(width / 2 - 100, height / 2 + 20);
+  timePicker.size(200, 30);
+  timePicker.input(() => selectedTime = timePicker.value());
 }
 
 function mousePressed() {
@@ -134,41 +145,15 @@ function mousePressed() {
       }
     }
   } else if (showDateTimePicker) {
-    let dBox = { x: width / 2 - 100, y: height / 2 - 40, w: 200, h: 40 };
-    let tBox = { x: width / 2 - 100, y: height / 2 + 20, w: 200, h: 40 };
-
-    if (isMouseOver(dBox.x, dBox.y, dBox.w, dBox.h)) {
-      typingDate = true;
-      typingTime = false;
-    } else if (isMouseOver(tBox.x, tBox.y, tBox.w, tBox.h)) {
-      typingTime = true;
-      typingDate = false;
-    } else if (isMouseOver(onaylaButtonBox.x, onaylaButtonBox.y, onaylaButtonBox.w, onaylaButtonBox.h)) {
+    if (isMouseOver(onaylaButtonBox.x, onaylaButtonBox.y, onaylaButtonBox.w, onaylaButtonBox.h)) {
       if (selectedDate && selectedTime) {
         sendTelegramMessage(selectedLocation, selectedDate, selectedTime);
       }
-    } else {
-      typingDate = false;
-      typingTime = false;
     }
   }
 }
 
-function keyTyped() {
-  if (typingDate) {
-    if (keyCode === BACKSPACE || key === '\b') {
-      selectedDate = selectedDate.slice(0, -1);
-    } else {
-      selectedDate += key;
-    }
-  } else if (typingTime) {
-    if (keyCode === BACKSPACE || key === '\b') {
-      selectedTime = selectedTime.slice(0, -1);
-    } else {
-      selectedTime += key;
-    }
-  }
-}
+function keyTyped() {}
 
 function isMouseOver(x, y, w = btnWidth, h = btnHeight) {
   return mouseX > x && mouseX < x + w && mouseY > y && mouseY < y + h;
